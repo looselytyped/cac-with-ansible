049944989baf1f8f776447dd6faa83fc25eb431c
Raju Gandhi <raju.gandhi@gmail.com>
2024-07-07 05:50:57 -0400

    feat: starting point for attendees

---
+++ _cac-with-ansible-backend/src/main/java/com/looselytyped/cac_with_ansible_backend/CacWithAnsibleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.looselytyped.cac_with_ansible_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacWithAnsibleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacWithAnsibleBackendApplication.class, args);
+	}
+
+}
